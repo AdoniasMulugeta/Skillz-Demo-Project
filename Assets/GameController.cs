e452650680558e77e9f4cac279046410ebe651c7
Adonias Mulugeta <mulugeta.adonias@gmail.com>
2019-12-12 14:11:18 +0300

    Initial Commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using SkillzSDK;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameController : SkillzMatchDelegate
+{
+    private const string GAME_SCENE_NAME = "Game Scene";
+    private const string MENU_SCENE_NAME = "Menu Scene";
+    
+    public void OnMatchWillBegin(Match matchInfo)
+    {
+        SceneManager.LoadScene(GAME_SCENE_NAME);    
+    }
+
+    public void OnSkillzWillExit()
+    {
+        SceneManager.LoadScene(MENU_SCENE_NAME);
+    }
+}
---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class MainMenuManager : MonoBehaviour
+{
+    public void LaunchSkillz()
+    {
+        SkillzCrossPlatform.LaunchSkillz(new GameController());
+    }
+
+    private void Start()
+    {
+        LaunchSkillz();
+    }
+}
